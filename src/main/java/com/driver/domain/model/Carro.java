d92bef428c35e6f8e7605cafedddc509ffaa9352
Guilherme Diniz <guilhermeplays03@gmail.com>
2023-09-07 13:26:30 -0300

    Criando a primeira entidade 'Carro'

---
+++ src/main/java/com/driver/domain/model/Carro.java
@@ -0,0 +1,53 @@
+package com.driver.domain.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+public class Carro {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(nullable = false)
+	private String modelo;
+	
+	@Column(nullable = false)
+	private Integer ano;
+	
+	@Column(nullable = false)
+	private Double preco;
+	
+	@Column(nullable = false)
+	private Double potencia;
+	
+	@Column(nullable = false)
+	private String cor;
+	
+	@Column(nullable = false)
+	private String carroceria;
+	
+	@Column(nullable = false)
+	private String combustivel;
+	
+	@Column(nullable = false)
+	private String cambio;
+	
+	@Column(nullable = false)
+	private String placa;
+	
+	@Column(nullable = false)
+	private String numeroChassi;
+	
+	@Column(nullable = false)
+	private String descricao;
+	
+}
