a224fe228bc90053df0e215ded2194adfa2f4d60
Guilherme Diniz <guilhermeplays03@gmail.com>
2023-09-14 16:07:46 -0300

    Implementando FindById de Fabricante

--- src/main/java/com/driver/api/controller/FabricanteController.java
+++ src/main/java/com/driver/api/controller/FabricanteController.java
@@ -4,11 +4,13 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.driver.domain.model.Fabricante;
 import com.driver.domain.repository.FabricanteRepository;
+import com.driver.domain.service.FabricanteService;
 
 @RestController
 @RequestMapping("/fabricantes")
@@ -17,9 +19,17 @@ public class FabricanteController {
 	@Autowired
 	private FabricanteRepository fabricanteRepository;
 	
+	@Autowired
+	private FabricanteService fabricanteService;
+	
 	@GetMapping
 	public List<Fabricante> findAll() {
 		return fabricanteRepository.findAll();
+	}
+	
+	@GetMapping("/{fabricanteId}")
+	public Fabricante findById(@PathVariable Long fabricanteId) {
+		return fabricanteService.buscarOuFalhar(fabricanteId);
 	}
 
 }
